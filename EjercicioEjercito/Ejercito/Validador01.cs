cdcb9c8e7e9e3dbb9b488e4cbfab178cffe74c94
Andrés Aragón <aragonandresjulian@gmail.com>
2024-05-15 10:27:44 +0200

    Se agregó una interfaz validadora de las divisiones

---
+++ EjercicioEjercito/Ejercito/Validador01.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjercicioEjercito.Ejercito
+{
+    public class Validador01 : IValidador
+    {
+        public bool isValid(IDivision division)
+        {
+            return (division.blindaje > 0 &&
+                division.capacidadDestruccion > 0 &&
+                division.capacidadMovimiento > 0 &&
+                division.nombreUnidad != null &&
+                division.potenciaFuego > 0 &&
+                division.precio > 0 &&
+                division.velocidad > 0);
+        }
+    }
+}

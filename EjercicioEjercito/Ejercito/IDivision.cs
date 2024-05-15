c9abaa1851193345b8c29a9d3969838a677f5a08
Andrés Aragón <aragonandresjulian@gmail.com>
2024-05-15 09:52:02 +0200

    Agregar archivos de proyecto.

---
+++ EjercicioEjercito/Ejercito/IDivision.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjercicioEjercito.Ejercito
+{
+    public interface IDivision
+    {
+        string nombreUnidad { get; set; }
+        double velocidad { get; set; }
+        double blindaje { get; set; }
+        double potenciaFuego { get; set; }
+        double precio { get; set; }
+        double capacidadDestruccion { get; set; }
+        double capacidadMovimiento { get; set; }
+        double resistenciaAtaque { get; set; }
+
+        double evaluarCapacidadMilitar();
+    }
+}
---
+++ EjercicioEjercito/Ejercito/Infanteria.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjercicioEjercito.Ejercito
+{
+    public class Infanteria : IDivision
+    {
+        // PROPIEDADES
+        public string nombreUnidad { get; set; }
+        public double velocidad { get; set; }
+        public double blindaje { get; set; }
+        public double potenciaFuego { get; set; }
+        public double precio { get; set; }
+        public double capacidadDestruccion { get; set; }
+        public double capacidadMovimiento { get; set; }
+        public double resistenciaAtaque { get; set; }
+        // FUNCIONES
+        public double evaluarCapacidadMilitar()
+        {
+            double capacidadMilitar = (potenciaFuego * capacidadMovimiento / 2) / (100 - blindaje);
+            return capacidadMilitar;
+        }// fin de evaluarCapacidadMilitar
+    }
+}
